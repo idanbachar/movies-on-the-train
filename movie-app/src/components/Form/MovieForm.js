949e7ad8cf9f5c01cc3b703769cce9321cbd8183
Idan Bachar <idanac2@gmail.com>
2021-07-21 20:03:07 +0300

    added validation for MovieForm component

--- movie-app/src/components/Form/MovieForm.js
+++ movie-app/src/components/Form/MovieForm.js
@@ -13,6 +13,54 @@ export default function MovieForm({ handler, titleLabel, movie }) {
     const [poster_path, setPosterPath] = useState(movie === undefined ? '' : movie.poster_path);
     const [release_date, setReleaseDate] = useState(movie === undefined ? new Date() : new Date(movie.release_date));
 
+    const [validateTitleMessage, setValidateTitleMessage] = useState('');
+    const [validatOverviewMessage, setValidateOverviewMessage] = useState('');
+    const [validatePosterPathMessage, setValidatePosterPathMessage] = useState('');
+
+    const validateFields = () => {
+
+        let isTitleValid = false;
+        let isOverviewValid = false;
+        let isPosterPathValid = false;
+
+        if (title.length < 4) {
+            isTitleValid = false;
+            setValidateTitleMessage("Title need to contain atleast 4 letters");
+        }
+        else {
+            isTitleValid = true;
+            setValidateTitleMessage("");
+        }
+        if (overview.length < 15) {
+            isOverviewValid = false;
+            setValidateOverviewMessage("Description need to contain atleast 15 letters");
+        }
+        else {
+            isOverviewValid = true;
+            setValidateOverviewMessage("");
+        }
+        if (poster_path.length < 8) {
+            isPosterPathValid = false;
+            setValidatePosterPathMessage("image need to contain atleast 8 letters");
+        }
+        else {
+            isPosterPathValid = true;
+            setValidatePosterPathMessage("");
+        }
+
+        if (isTitleValid &&
+            isOverviewValid &&
+            isPosterPathValid) {
+            handler({
+                id,
+                title,
+                overview,
+                poster_path,
+                release_date: release_date.toISOString().split('T')[0]
+            })
+        }
+    };
+
     return (
         <div className="detailed-card">
             <div className="card-body">
@@ -26,7 +74,7 @@ export default function MovieForm({ handler, titleLabel, movie }) {
                             onChange={(e) => setTitle(e.target.value)}
                         />
                         <Form.Text className="text-muted">
-                            Select good name for movie, to make people watch.
+                            <font color="red">{validateTitleMessage}</font>
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -38,16 +86,21 @@ export default function MovieForm({ handler, titleLabel, movie }) {
                             value={overview}
                             onChange={(e) => setOverview(e.target.value)}
                         />
+                        <Form.Text className="text-muted">
+                            <font color="red">{validatOverviewMessage}</font>
+                        </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Image (from URL)</Form.Label>
-
                         <Form.Control
                             type="text"
                             placeholder="Enter movie cover image"
                             value={poster_path}
                             onChange={(e) => setPosterPath(e.target.value)}
                         />
+                        <Form.Text className="text-muted">
+                            <font color="red">{validatePosterPathMessage}</font>
+                        </Form.Text>
                         <br />
                         <Form.Text className="text-muted">
                             Preview image:
@@ -55,7 +108,7 @@ export default function MovieForm({ handler, titleLabel, movie }) {
                         <br />
                         <img
                             src={poster_path}
-                            alt={title}
+                            alt=""
                             width={150}
                         />
                     </Form.Group>
@@ -64,18 +117,12 @@ export default function MovieForm({ handler, titleLabel, movie }) {
                         <br />
                         <DatePicker selected={release_date} onChange={(date) => setReleaseDate(date)} />
                     </Form.Group>
-                    <Button variant="primary" onClick={() => handler({
-                        id,
-                        title,
-                        overview,
-                        poster_path,
-                        release_date: release_date.toISOString().split('T')[0]
-                    })}>
+                    <Button variant="primary" onClick={validateFields}>
                         {titleLabel}!
                     </Button>
                     <Link
                         className="btn btn-danger"
-                        to={`/details/${id}`}>
+                        to={titleLabel === "Edit" ? `/details/${id}` : `/`}>
                         Cancel
                     </Link>
                 </Form>
